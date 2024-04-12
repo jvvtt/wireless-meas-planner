5877a6f951f73a7b200e7955a210d14fb9d7c112
Julian Villegas <julian.villegas@vtt.fi>
2024-04-12 17:51:36 +0300

    add: GOSA functions in scheduler.js

--- src/logic/scheduler.js
+++ src/logic/scheduler.js
@@ -1,7 +1,7 @@
 import { ACTION_TYPES, ACTORS } from "../constants/constants";
 
 // Group of ordered simultaneous actions (GOSA)
-GOSA = [
+const GOSA = [
   [
     {
       ACTION_NAME: ACTION_TYPES.OBSERVER_OPERATOR.REPORT_AIRCRAFT.NAME,
@@ -57,9 +57,22 @@ GOSA = [
 ];
 
 export function findSiblingsInGOSA(actionName) {
-  return 1;
+  const idxsSiblings = findIdxInGOSA(actionName);
+  return GOSA.filter((SAO, cnt) => idxsSiblings[cnt] !== false);
 }
 
-export function findIdxInGOSA() {
-  return 1;
+export function findIdxInGOSA(actionName) {
+  return GOSA.map((SAO, cnt) => {
+    const AO = SAO.filter(
+      (actionObject) => actionObject.ACTION_NAME === actionName
+    );
+    // The length of AO should be exactly 1, as an AO must not be repeated in a single SAO.
+    if (AO.length === 1) {
+      return cnt;
+    } else if (AO.length > 1) {
+      console.log("DEBUG: Multiple AO of the same type in a single SAO");
+    } else {
+      return false;
+    }
+  });
 }
